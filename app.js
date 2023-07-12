0605ac12a13e317533f2b9422bb570e6adbb51b5
fornara <fornarapro@gmail.com>
2023-07-12 15:30:49 +0300

    swapKeysValues
    упражнение на свап ключей и значений
    (циклом сам догадался)

--- app.js
+++ app.js
@@ -16,40 +16,75 @@
 // console.log([...setFlights]);//получили уникальное значение из setFlights и добавили его в массив
 //уникализировать объекты в массиве с помощью set не получится, так как это не простейшие элементы массива!!
 
-const wetherMap = new Map();
-wetherMap
-    .set('London', '10')
-    .set('Moscow', '5');
+// const wetherMap = new Map();
+// wetherMap
+//     .set('London', '10')
+//     .set('Moscow', '5');
 
-console.log(wetherMap.get('London'));
-console.log(wetherMap.has('London'));
-wetherMap.delete('Moscow');
-// wetherMap.clear();
+// console.log(wetherMap.get('London'));
+// console.log(wetherMap.has('London'));
+// wetherMap.delete('Moscow');
+// // wetherMap.clear();
 
-wetherMap
-   .set(1, 5)
-   .set(true, 'yes')
-   .set(true, 'yes!')
-   .set(false, 'no')
-   .set([1, 2, 3, 4, 5], 'array')
-   .set({a: 3}, {a: 3});
-
-
-console.log(wetherMap);
+// wetherMap
+//    .set(1, 5)
+//    .set(true, 'yes')
+//    .set(true, 'yes!')
+//    .set(false, 'no')
+//    .set([1, 2, 3, 4, 5], 'array')
+//    .set({a: 3}, {a: 3});
 
 
+// console.log(wetherMap);
 
-const wetherMap2 = new Map([
-    ['London', '10'],
-    ['Moscow', '102'],
+
+
+// const wetherMap2 = new Map([
+//     ['London', '10'],
+//     ['Moscow', '102'],
+// ]);
+// console.log(wetherMap2);
+
+// const wetherObj = {
+//     london: '10',
+//     moscow: '4',
+//     saratov: '3'
+// };
+// console.log(Object.entries(wetherObj));
+// const wetherMap3 = new Map(Object.entries(wetherObj));
+// console.log(wetherMap3);
+
+
+
+//////////////////////////иттерация мапов
+// const weatherMap = new Map([
+//     ['London', '10'],
+//     ['Moscow', '4'],
+//     ['Paris', '42'],
+// ]);
+
+// for(const [key, value] of weatherMap) {
+//     console.log(key);
+//     console.log(value);
+// }
+
+// console.log([...weatherMap]);
+// console.log([...weatherMap.keys()]);
+// console.log([...weatherMap.values()]);
+////////////////////////////////////
+
+////меняем местами ключ и значение/////
+let weatherMap = new Map([
+    ['London', 10],
+    ['Moscow', 4],
+    ['Paris', 42],
 ]);
-console.log(wetherMap2);
+const newMap = [];
+for (const [key, value] of weatherMap) { 
+     newMap.push([value, key]);//в сола сам догадался
+};    
 
-const wetherObj = {
-    london: '10',
-    moscow: '4',
-    saratov: '3'
-};
-console.log(Object.entries(wetherObj));
-const wetherMap3 = new Map(Object.entries(wetherObj));
-console.log(wetherMap3);
+console.log(newMap);
+
+weatherMap = new Map([...weatherMap].map(el => el.reverse()));//ну так да конечно проще, но я не знал
+console.log(weatherMap);
