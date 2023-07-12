012267f6c547c6cd7b59238cba4613a4defb7e8c
fornara <fornarapro@gmail.com>
2023-07-12 13:46:13 +0300

    setFlights
    что такое set и зачем оно нужно

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 "use strict"
+const flights = ['Russia', 'Mexico', 'Canada', 'Mexico', 'Canada'];
+const setFlights = new Set(flights);
+console.log(setFlights);
+console.log(setFlights.size);
+console.log(setFlights.has('Russia'));
+setFlights.add('Spain');
+console.log(setFlights);
+setFlights.delete('Canada');
+console.log(setFlights);
+//перебираем все элементы массива
+for (const flights of setFlights) {
+    console.log(flights);
+}
 
-console.log("hello world");
+console.log([...setFlights]);//получили уникальное значение из setFlights и добавили его в массив
+//уникализировать объекты в массиве с помощью set не получится, так как это не простейшие элементы массива!!
